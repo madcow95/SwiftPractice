e944d535d0246166526ae265ded6d0176feb1ad8
madcow95 <lobasketve@naver.com>
2023-01-24 21:18:38 +0900

    접근수준 내용 추가

--- day_12.playground/Contents.swift
+++ day_12.playground/Contents.swift
@@ -106,3 +106,80 @@ struct AnotherType {
 
 var anotherInstance = AnotherType()
 anotherInstance.someMethod()
+
+// 설정자의 접근수준 지정
+public struct SomeType2 {
+    // 비공개 접근수준 저장 Property count
+    private var count: Int = 0
+    
+    // 공개 접근수준 저장 Property publicStoredProperty
+    public var publicStoredProperty: Int = 0
+    
+    // 공개 접근수준 저장 Property publicGetOnlyStoredProperty
+    // 설정자(Setter)는 비공개 접근수준
+    public private(set) var publicGetOnlyStoredProperty: Int = 0
+    
+    // 내부 접근수준 저장 Property internalComputedProperty
+    internal var internalComputedProperty: Int {
+        get {
+            return count
+        }
+        set {
+            count += 1
+        }
+    }
+    
+    // 내부 접근수준 저장 Property internalGetOnlyComputedProperty
+    // 설정자(setter)는 비공개 접근수준
+    internal private(set) var internalGetOnlyComputedProperty: Int {
+        get {
+            return count
+        }
+        set {
+            count += 1
+        }
+    }
+    
+    // 공개 접근수준 서브스크립트
+    public subscript() -> Int {
+        get {
+            return count
+        }
+        set {
+            count += 1
+        }
+    }
+    
+    // 공개 접근수준 서브스크립트
+    // 설정자(Setter)는 내부 접근수준
+    public internal(set) subscript(some: Int) -> Int {
+        get {
+            return count
+        }
+        set {
+            count += 1
+        }
+    }
+}
+
+var someInstance: SomeType2 = SomeType2()
+
+// 외부에서 접근자(Getter), 설정자(Setter)모두 사용 가능
+print(someInstance.publicStoredProperty) // 0
+someInstance.publicStoredProperty = 100
+
+// 외부에서 접근자만 사용 가능
+print(someInstance.publicGetOnlyStoredProperty)
+// someInstance.publicGetOnlyStoredProperty = 100 -> 에러 발생
+
+// 외부에서 접근자, 설정자 모두 사용 가능
+print(someInstance.internalComputedProperty)
+someInstance.internalComputedProperty = 100
+
+// 외부에서 접근자만 사용 가능
+print(someInstance.internalGetOnlyComputedProperty)
+// someInstance.internalGetOnlyComputedProperty = 100 -> 에러 발생
+
+// 외부에서 접근자, 설정자 모두 사용 가능
+print(someInstance[])
+someInstance[] = 100
